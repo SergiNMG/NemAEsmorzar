57dc36b4228eef1e5e9832d9b11c8611a605f05e
GOMEZ Sergio Nicolas <sn.martinez@ext.soprasteria.com>
2024-05-01 22:56:39 +0200

    Firebase added to the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainProductComponent } from './modules/product/components/main-product/main-product.component';
 import { ProductsComponent } from './modules/product/components/products/products.component';
 import { ReviewsComponent } from './modules/place/components/reviews/reviews.component';
+import { environment } from './environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 
 @NgModule({
   declarations: [
@@ -18,9 +23,13 @@ import { ReviewsComponent } from './modules/place/components/reviews/reviews.com
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
