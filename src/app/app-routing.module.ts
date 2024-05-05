af56ef3be21aef613face1950ade603793fc014e
GOMEZ Sergio Nicolas <sn.martinez@ext.soprasteria.com>
2024-05-05 16:49:29 +0200

    app rounting-module implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'places', pathMatch: 'full' },
+  {
+    path: 'places',
+    loadChildren: () =>
+      import('./modules/place/place.module').then((m) => m.PlaceModule),
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./modules/product/product.module').then((m) => m.ProductModule),
+  },
+  {
+    path: 'about',
+    loadChildren: () =>
+      import('./modules/about/about.module').then((m) => m.AboutModule),
+  },
+  { path: '**', redirectTo: 'places', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
